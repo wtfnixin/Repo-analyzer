bbaed07d0c8596309356f8dedbf76e1dce5eded2
wtfnixin <nitinkumardontam@gmail.com>
2026-04-13 14:17:00 +0530

    Update CORS configuration for Vercel and localhost origins

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: [
+    "http://localhost:5173",
+    "https://repo-analyzer-pearl.vercel.app"
+  ]
+}));
 app.use(express.json());
 
 app.use('/api/github', require('./routes/githubRoutes'));
